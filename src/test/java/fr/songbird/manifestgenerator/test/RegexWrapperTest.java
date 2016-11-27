dcdf17e0e9983d20bedd6f7a0a4478ab0b8f0d9f
Defranceschi Anthony <chaacygg@gmail.com>
2016-11-27 10:57:11 +0100

    0.4.1
    Refactoring sur le premier test.
    
    Ajout de l'implémentation du test RegexWrapper_overloaded_constructor_test().

--- src/test/java/fr/songbird/manifestgenerator/test/RegexWrapperTest.java
+++ src/test/java/fr/songbird/manifestgenerator/test/RegexWrapperTest.java
@@ -15,14 +15,23 @@ public class RegexWrapperTest {
 	@Test
 	public void RegexWrapper_instanciation_test() 
 	{
-		final RegexWrapper rw = new RegexWrapper("\\w", 0);
-		assertThat("This test is broken !",rw.get_m_regex().pattern(), is("\\w"));
+		final String PATTERN_PARAMETER = "\\w";
+		final RegexWrapper RW = new RegexWrapper(PATTERN_PARAMETER, 0);
+		final String PATTERN_STRING = RW.get_m_regex().pattern();
+		assertThat("PATTERN_STRING isn't equals to "+ PATTERN_PARAMETER 
+				+ "\nDebug: PATTERN_STRING == '" 
+				+ PATTERN_STRING 
+				+ "'", 
+				PATTERN_STRING, is(PATTERN_PARAMETER));
 	}
 	
 	@Test
 	public void RegexWrapper_overloaded_constructor_test()
 	{
-		
+		final String PATTERN_PARAMETER = "\\w";
+		final RegexWrapper RW = new RegexWrapper(PATTERN_PARAMETER);
+		final int FLAG_RETURN = RW.get_m_flag();
+        assertThat("Erreur, la méthode get_m_flag() a renvoyé: " + FLAG_RETURN, FLAG_RETURN, is(0));
 	}
 
 }
