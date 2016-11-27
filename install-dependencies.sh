ca2d0706968d0b8cc6db16b06445c32de431ccaf
Defranceschi Anthony <chaacygg@gmail.com>
2016-11-27 14:34:28 +0100

    maj installer

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -6,6 +6,3 @@ echo "## Installation des dépendances qui ne disposent pas de pom.xml. ##"
 mvn install:install-file -Dfile=src/main/resources/WyLog_v2.jar -DgroupId=net.wytrem.logging -DartifactId=wylog -Dversion=2.0 -Dpackaging=jar
 mvn install -X
 echo "## Installation terminée. ##"
-echo "## Compilation en cours... ##"
-mvn compile -X
-echo "## Compilation terminée. ##"
