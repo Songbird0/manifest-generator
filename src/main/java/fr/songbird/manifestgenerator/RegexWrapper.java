efbfd475423a8b11258cdcac76d1e5ac5b03c370
Defranceschi Anthony <chaacygg@gmail.com>
2016-11-27 08:51:44 +0100

    update RegexWrapper
    Implémentation du constructeur de la classe.
    
    Ajout de la documentation dédiée à l'implémentation.

--- src/main/java/fr/songbird/manifestgenerator/RegexWrapper.java
+++ src/main/java/fr/songbird/manifestgenerator/RegexWrapper.java
@@ -4,17 +4,32 @@
 package fr.songbird.manifestgenerator;
 
 /**
+ * Wrapper encapsulant les classes Pattern et Matcher.
  * @author songbird
- *
+ * @version 0.1.0
+ * @since 27 nov. 2016
+ * @see java.util.regex.Pattern Pattern
+ * @see java.util.regex.Matcher Matcher
+ * 
  */
 public class RegexWrapper {
 
+	private final String m_pattern;
 	/**
-	 * 
+	 * La classe RegexWrapper ne possède qu'un seul constructeur muni d'un seul paramètre.
+	 * <p>
+	 * Vous pouvez lui passer en paramètre la chaîne de caractères que vous souhaitez faire compiler à votre automate.
+	 * <p>
+	 * Une fois passé en paramètre, {@code pattern} sera utilisé pour instancier un objet de la classe Pattern, 
+	 * puis compilé par un objet Matcher.
+	 * @param pattern Votre chaîne de caractères.
 	 */
-	public RegexWrapper() 
+	public RegexWrapper(final String pattern) 
 	{
-		
+		assert(pattern != null) : "pattern binding is null, abort the mission, chief !";
+		assert(!pattern.isEmpty()) : "pattern string is null, abort the mission, chief !";
+//		Pourquoi utiliser ce wrapper avec une chaîne de caractères vide ?
+		m_pattern = pattern;
 	}
 
 }
