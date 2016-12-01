462ec6f05c539bbb6f732d49973878dc5cc50981
Defranceschi Anthony <chaacygg@gmail.com>
2016-12-01 22:24:22 +0100

    0.7.1
    Rédaction d'un nouveau test pour vérifier la somme des flags passés en paramètre.

--- src/test/java/fr/songbird/manifestgenerator/test/RegexWrapperTest.java
+++ src/test/java/fr/songbird/manifestgenerator/test/RegexWrapperTest.java
@@ -33,4 +33,15 @@ public class RegexWrapperTest {
         assertThat("Erreur, la méthode get_m_flag() a renvoyé: " + FLAG_RETURN, FLAG_RETURN, is(0));
 	}
 
+	@Test
+	public void RegexWrapper_flagsum_test()
+	{
+		final int FLAG_SUM = Pattern.MULTILINE;
+		final RegexWrapper RW = new RegexWrapper("^(\\w)+", FLAG_SUM);
+		final int FLAG_RETURN = RW.get_m_flag();
+		assertThat("Erreur, la somme est égale à " 
+		+ FLAG_RETURN 
+		+ " alors qu'elle devrait être égale à " 
+		+ FLAG_SUM + ".", FLAG_RETURN, is(FLAG_SUM));
+	}
 }
